eb734195212604b35432167cbe3aa6d58b71ccd6
Marwan <Marwanmoataz123@gmail.com>
2024-02-14 18:32:20 +0200

    initial commit

---
+++ app/Http/Controllers/API/v1/Auth/RegisterController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\API\v1\Auth;
+
+use App\Helpers\ResponseError;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Services\AuthService\AuthByEmail;
+use App\Services\AuthService\AuthByMobilePhone;
+use App\Traits\ApiResponse;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    use ApiResponse;
+    
+    public function testapi(Request $request){
+        return response()->json($request->id);
+    }
+    public function register(RegisterRequest $request): JsonResponse
+    {
+        if ($request->input('phone')) {
+
+            return (new AuthByMobilePhone)->authentication($request->validated());
+
+        } else if ($request->input('email')) {
+
+            return (new AuthByEmail)->authentication($request->validated());
+
+        }
+
+        return $this->onErrorResponse([
+            'code' => ResponseError::ERROR_400
+        ]);
+    }
+}
---
+++ config/credential.php
@@ -0,0 +1,8 @@
+<?php
+ return [
+
+'purchase_id' => '45828592',
+
+'purchase_code' => '41ae95a5-5c40-4736-a8a1-aa2250de8f4b',
+
+];
